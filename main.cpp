2f14d79c773310bdc0776cb34d0f6210c592432d
¨Higginson¨ <higgins.matias@icloud.com>
2026-07-25 20:16:13 -0500

    window render

--- main.cpp
+++ main.cpp
@@ -7,49 +7,48 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 }
 
 int main() {
-    // Inicializar GLFW
-    if (!glfwInit()) {
-        std::cout << "Fallo al inicializar GLFW" << std::endl;
+
+    const int resx = 1440;
+    const int resy = 1080;
+
+    //Initialize GLFW
+    if (!glfwInit ()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
 
-    // Configurar GLFW para usar OpenGL 3.3 Core Profile (Compatible con Mac)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Requisito para macOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //requirement for macOs
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Mi Primer Shooter - Motor Propio", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(resx, resy, "Shooter", nullptr, nullptr);
+
     if (window == nullptr) {
-        std::cout << "Fallo al crear la ventana GLFW" << std::endl;
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Cargar los punteros de OpenGL con GLAD
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Fallo al inicializar GLAD" << std::endl;
+    //Initialize GLAD to manage function pointers
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // Bucle principal del juego (Game Loop)
+    //GameLoop
     while (!glfwWindowShouldClose(window)) {
-        // Procesar entradas del teclado (Ej: presionar ESC para cerrar)
-        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, true);
+        }
 
-        // Renderizado: Pintar el fondo de un color azul oscuro
-        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Intercambiar buffers y procesar eventos
         glfwSwapBuffers(window);
-        glfwPollEvents();
+        glfwPollEvents(); //this function checks if any event is triggered
     }
 
-    // Limpiar recursos y cerrar
     glfwTerminate();
     return 0;
 }
