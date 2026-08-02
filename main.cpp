5eed52bd80d505544f0e1507c2059f8f8e50628d
¨Higginson¨ <higgins.matias@icloud.com>
2026-08-02 18:16:22 -0500

    headers and cpp class

--- main.cpp
+++ main.cpp
@@ -9,9 +9,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 }
 
 int main() {
-
-    const int resx = 1440;
-    const int resy = 1080;
+    constexpr int resx = 1440;
+    constexpr int resy = 1080;
 
     //Initialize GLFW
     if (!glfwInit ()) {
@@ -27,7 +26,7 @@ int main() {
     GLFWwindow* window = glfwCreateWindow(resx, resy, "Shooter", nullptr, nullptr);
 
     if (window == nullptr) {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        std::cout << "[ERROR]: Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -40,6 +39,39 @@ int main() {
         return -1;
     }
 
+    Shader myShader("shaders/basico.vert", "shaders/basico.frag");
+
+    float vertices [] {
+         0.5f,  0.5f, 0.0f,  // Arriba a la derecha     (Índice 0)
+         0.5f, -0.5f, 0.0f,  // Abajo a la derecha      (Índice 1)
+        -0.5f, -0.5f, 0.0f,  // Abajo a la izquierda    (Índice 2)
+        -0.5f,  0.5f, 0.0f   // Arriba a la izquierda   (Indice 3)
+    };
+
+    unsigned int indices [] {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
     //GameLoop
     while (!glfwWindowShouldClose(window)) {
 
@@ -47,9 +79,22 @@ int main() {
             glfwSetWindowShouldClose(window, true);
         }
 
+        myShader.use();
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        myShader.setVec4("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+
         glfwSwapBuffers(window);
         glfwPollEvents(); //this function checks if any event is triggered
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(myShader.ID);
 
     glfwTerminate();
     return 0;
